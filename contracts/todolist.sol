0700b355674f55ba76a78006a14180653ef53c4f
piio-zicko <isaacijuo@gmail.com>
2022-08-23 16:01:37 +0000

    Initial To-Do-List Commit

---
+++ contracts/todolist.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract ToDoList {
+struct Todo {
+    string message;
+    bool taskCompleted;
+
+}
+
+Todo[] public todos;
+
+function createToDo(string calldata _message) external {
+    todos.push(Todo ( {
+        message: _message,
+        taskCompleted: false
+
+
+    }));
+
+
+}
+
+function updateToDo(uint _index, string calldata _message) external {
+    todos[_index].message = _message;
+}
+
+function toggleCOmpleted(uint _index) external {
+    todos[_index].taskCompleted = !todos[_index].taskCompleted;
+
+}
+
+}
+
+
+
